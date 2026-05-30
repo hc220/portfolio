1efbac7c89c81b8ddfbb88093c53d7725dbec84f
Harshil Patel <harshil220205@gmail.com>
2026-05-30 21:38:58 +0530

    again commit

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -158,7 +158,7 @@ const experiences = [
               className="glass-card p-5 rounded-2xl"
             >
               <div className="flex items-center gap-3">
-                <Award className="w-5 h-5 text-primary" />
+             
                 <span>{item}</span>
               </div>
             </div>
